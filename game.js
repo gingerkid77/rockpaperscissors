94acb7be88b71b2e328c3797aa657685c883354b
Matt <17509428+gingerkid77@users.noreply.github.com>
2025-01-20 18:17:02 +0000

    Refactored to use button events and update HTML

--- game.js
+++ game.js
@@ -17,30 +17,33 @@ function getComputerChoice() {
     return options[Math.round(Math.random() * 2)]
 }
 
-function getHumanChoice() {
-
-    let input = ""
-
-    // Could modify this to tell the user if the input is wrong
-    while ((options.indexOf(input.toLowerCase()) === -1)) {
-        input = prompt("Make your choice : ", "")
-    }
-
-    return input
-}
 
 function playRound(humanChoice, computerChoice) {
 
-    console.log(`Human chose ${humanChoice}, Computer chose ${computerChoice}`)
+    document.querySelector("#message").textContent = `Human chose ${humanChoice}, Computer chose ${computerChoice}`
 
-    if (humanChoice === computerChoice) {
-        console.log('No score draw')
+
+    if( humanChoice === computerChoice ) {
+        document.querySelector("#message").textContent = document.querySelector("#message").textContent + ', no score draw'
     }
     else if (winningMoves[humanChoice] === computerChoice) {
         humanScore = humanScore + 1
+        document.querySelector("#message").textContent = document.querySelector("#message").textContent + ', human scores'
     }
     else {
+        
         computerScore = computerScore + 1
+        document.querySelector("#message").textContent = document.querySelector("#message").textContent + ', computer scores'
+    }
+
+    document.querySelector("#human").textContent = humanScore
+    document.querySelector("#computer").textContent = computerScore
+
+    if (computerScore == 5) {
+        document.querySelector("#message").textContent = 'Computer wins.'
+    }
+    if (humanScore == 5) {
+        document.querySelector("#message").textContent = 'Human wins.'
     }
 }
 
@@ -54,16 +57,26 @@ function playGame(rounds) {
         console.log(`Human : ${humanScore}, Computer : ${computerScore}`)
     }
 
-    if (humanScore === computerScore) {
-        console.log("It's a draw")
-    }
-    else if (humanScore > computerScore) {
-        console.log("Human wins")
-    }
-    else {
-        console.log("Computer wins")
-    }
+
 }
 
-// Could improve this to ask how many rounds to play
-playGame(5)
+
+document.addEventListener('click', (event) => {
+
+    switch (event.target.id) {
+
+        case 'rock':
+
+        case 'paper':
+
+        case 'scissors':
+            playRound(event.target.id, getComputerChoice())
+            break;
+
+        default:
+        // ignore the event
+
+    }
+
+
+})
